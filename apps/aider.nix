732cc2761c0624cee27c4ac14e8fd590ffd03aaf
Gianluca Brindisi <g@brindi.si>
2024-10-17 14:37:19 +0200

    feat: Bump aider version to 0.59.0 and add pydub to dependencies

--- apps/aider.nix
+++ apps/aider.nix
@@ -7,7 +7,7 @@ let
     packageOverrides = _: super: { tree-sitter = super.tree-sitter_0_21; };
   };
 
-  version = "0.57.0";
+  version = "0.59.0";
 
 in python3.pkgs.buildPythonApplication {
   pname = "aider-chat";
@@ -15,10 +15,11 @@ in python3.pkgs.buildPythonApplication {
   pyproject = true;
 
   src = pkgs.fetchFromGitHub {
-    owner = "paul-gauthier";
+    owner = "aider-ai";
     repo = "aider";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ErDepSju8B4GochHKxL03aUfOLAiNfTaXBAllAZ144M=";
+    # nix hash to-sri --type sha256 $(nix-prefetch-url --unpack https://github.com/Aider-AI/aider/archive/refs/tags/v0.59.0.tar.gz)
+    hash = "sha256-20LicYj1j5gGzhF+SxPUKu858nHZgwDF1JxXeHRtYe0=";
   };
 
   pythonRelaxDeps = true;
@@ -53,6 +54,7 @@ in python3.pkgs.buildPythonApplication {
     playwright
     prompt-toolkit
     ptyprocess
+    pydub
     pypager
     pypandoc
     pyperclip
